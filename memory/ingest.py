7692bad8a12bd1242b415a99aa4421e051728f9b
Hu, Senkang <74350191+Bmithbs@users.noreply.github.com>
2023-06-13 20:59:24 +0800

    update

--- memory/ingest.py
+++ memory/ingest.py
@@ -5,16 +5,18 @@ from pathlib import Path
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import TextLoader
 import time 
+from utils import config_util as cfg
+openai_api_key = cfg.key_chatgpt_api_key
 
 def ingest(memory_path="raw_memory/lsc.txt", database_path="./"):
 # Here we load in the data in the format that Notion exports it in.
-    embedding = OpenAIEmbeddings(openai_api_key='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')
+    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
     loader = TextLoader(memory_path) # source
     documents = loader.load()
     # Here we split the documents, as needed, into smaller chunks.
     # We do this due to the context limits of the LLMs.
-    text_splitter = CharacterTextSplitter(chunk_size=1000, separator="\n")
+    text_splitter = CharacterTextSplitter(chunk_size=500, separator="\n")
     docs = text_splitter.split_documents(documents)
     persist_directory=database_path
     print("Start to ingest!")
